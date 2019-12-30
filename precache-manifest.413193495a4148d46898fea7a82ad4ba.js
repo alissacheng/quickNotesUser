addca059a3b4655bc92c90d64b3ad75c9e3a9504
Alissa <alissabc@hotmail.com>
2019-12-30 11:54:54 -0500

    Updates

---
+++ precache-manifest.413193495a4148d46898fea7a82ad4ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbad2f522ed756eae6fa83f85b1526e",
+    "url": "/quickNotesUser/index.html"
+  },
+  {
+    "revision": "604dfcbb39f106a15564",
+    "url": "/quickNotesUser/static/css/main.e5827207.chunk.css"
+  },
+  {
+    "revision": "62e59f3b9c4a2a3c5cf3",
+    "url": "/quickNotesUser/static/js/2.39c30ee0.chunk.js"
+  },
+  {
+    "revision": "882265108c00bb7da64da6a7e04f8a40",
+    "url": "/quickNotesUser/static/js/2.39c30ee0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "604dfcbb39f106a15564",
+    "url": "/quickNotesUser/static/js/main.9238a507.chunk.js"
+  },
+  {
+    "revision": "7dabe4ac464200d2894b",
+    "url": "/quickNotesUser/static/js/runtime-main.d396c6dc.js"
+  },
+  {
+    "revision": "a71f26e3f7a9b35c145deda90186d921",
+    "url": "/quickNotesUser/static/media/corkBoard.a71f26e3.jpg"
+  }
+]);
