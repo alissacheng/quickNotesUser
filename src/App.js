5dd53feace2bfaae4fd931de844fe892fb22ae65
Alissa <alissabc@hotmail.com>
2020-01-05 09:56:28 -0500

    added span and aria hidden for emoji

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ class App extends Component {
       document.getElementById("dialog").classList.add("visuallyHidden")
       document.getElementById("edit").removeAttribute("open")
       document.getElementById("edit").classList.add("visuallyHidden")
-
+//Clear user input and user id once dialog closes
       this.setState({
         userInput: "",
         userId:null
@@ -338,7 +338,10 @@ class App extends Component {
                         return(
                             <li key={i}>
                               <div className="titleBar">
-                                <button id={noteValue.noteId} value={noteValue.noteText} className="edit" title="Edit note" onClick={this.editNote}>📝 Edit</button>
+                                <button id={noteValue.noteId} value={noteValue.noteText} className="edit" title="Edit note" onClick={this.editNote}>
+                                  <span role="img" aria-label="notepad" aria-hidden="true">📝</span> 
+                                  Edit
+                                </button>
                                 <button id={noteValue.noteId} className="delete" onClick={this.deleteNote} title="Delete note" tabIndex="0">X</button>
                               </div>
                               <textarea rows="7" cols="16" value={noteValue.noteText} readOnly></textarea>
